52c879749507a861f788b20fb41df16353665226
vladislav900087 <fyn5yhsj4h@privaterelay.appleid.com>
2026-07-26 13:58:18 +0500

    Add user Pydantic schemas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr
+from backend.app.models.user import UserRole
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserRead(UserBase):
+    id: int
+    role: UserRole
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
